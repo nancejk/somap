c9cdeec00bea5e706819ccfa0fcc50668b210fd1
Jared Nance <jared.nance@gmail.com>
2010-06-17 14:24:19 -0700

    Dropped assert.h from includes and switched unit tests to new format

--- tests/somapconfig_tests.cpp
+++ tests/somapconfig_tests.cpp
@@ -1,7 +1,6 @@
 #include <somapconfig.hpp>
 #include <new>
 #include <memory>
-#include <assert.h>
 
 bool somapconfig_default_constructor( int arg_count, char** arg_vector ) {
   std::auto_ptr<somapconfig> default_config ( new (std::nothrow) somapconfig() );
@@ -12,6 +11,7 @@ bool somapconfig_default_constructor( int arg_count, char** arg_vector ) {
 }
 
 bool somapconfig_args_constructor( int arg_count, char** arg_vector ) {
+  //TODO this should actually perform checks based on input.
   std::auto_ptr<somapconfig> config( new (std::nothrow) somapconfig(arg_count, arg_vector) );
   return true;
 }
