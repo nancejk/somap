a22912f062fad6eb8997d20b97e284bf7ea0c94f
Jared Nance <jared.nance@gmail.com>
2010-06-18 14:50:29 -0700

    Accept daniel's changes to the functors.

--- inc/somapfunctors.hpp
+++ inc/somapfunctors.hpp
@@ -4,28 +4,14 @@
 #include <cmath>
 
 class somapfunctor {
-  // dansci-- commented next 3 lines out as i think () should be
-  // public
-//  virtual double operator()(somapnode lhs, somapnode rhs) = 0;
-// public:
-//  virtual ~somapfunctor();
-  // <dansci>
 public:
-  virtual double operator()(somapnode lhs, somapnode rhs) = 0;
+ virtual double operator()(somapnode lhs, somapnode rhs) = 0;
  virtual ~somapfunctor();
-  // </dansci>
 };
 
 class cartesian_distance: public somapfunctor {
-  // dansci-- again, i think () should be public.  commented next 3
-  // lines.
-//   virtual double operator()(somapnode lhs, somapnode rhs);
-// public:
-//   virtual ~cartesian_distance();
-  // <dansci>
 public:
   virtual double operator()(somapnode lhs, somapnode rhs);
   virtual ~cartesian_distance();
-  // </dansci>
 };
 #endif
